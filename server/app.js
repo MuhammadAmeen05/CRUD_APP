4f2b9d0caf934fa1d0b582c55752e5f0416151f0
Your Name <muhammadameenbirahmani6@gmail.com>
2023-05-17 13:42:19 +0500

    update

--- server/app.js
+++ server/app.js
@@ -1,24 +1,23 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
 const ConnectDB=require('./db/conn');
-ConnectDB();  
-const users = require("./models/userSchema");
+const path=require('path')
 const cors = require("cors");
 const router = require("./routes/router");
-
 const port = process.env.PORT || 8003;
-
+ConnectDB();  
 app.use(cors());
 app.use(express.json());
 
 app.get("/",(req,res)=>{
     res.json("server start")
 })
-
 app.use(router);
-
+app.use(express.static(path.join(__dirname,'/client/build')))
+app.get("*",function(req,res){
+    res.sendFile(path.join(__dirname,'/client/build/index.html'))
+})
 app.listen(port, () => {
     console.log(`server is start port number ${port}`);
 });
